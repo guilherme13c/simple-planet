3791d4c46c28380b57dad9e7d438d8ea14bad649
guilherme13c <guilhermesccaporali@protonmail.com>
2025-05-14 14:23:54 -0300

    refactor: simplify shader code and remove unused variables

--- src/main.zig
+++ src/main.zig
@@ -18,46 +18,7 @@ pub fn main() !void {
     var app = glfw_setup.glfwApp.init(800, 600, loop) orelse return;
     defer app.deinit();
 
-    const prog = try gl_shader_setup.shaderProgram.init(vertexShaderSrc, fragmentShaderSrc);
-    _ = prog;
-    const cube = try createCube();
-    _ = cube;
-
     app.run();
 
     return;
 }
-
-fn createCube() !struct {
-    vao: u32,
-    vbo: u32,
-    ebo: u32,
-} {
-    const vao: u32 = 0;
-    const vbo: u32 = 0;
-    const ebo: u32 = 0;
-
-    return .{ .vao = vao, .vbo = vbo, .ebo = ebo };
-}
-
-const cubeVertices = [_]f32{
-    // positions       // colors
-    -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
-    0.5,  -0.5, -0.5, 0.0, 1.0, 0.0,
-    0.5,  0.5,  -0.5, 0.0, 0.0, 1.0,
-    -0.5, 0.5,  -0.5, 1.0, 1.0, 0.0,
-    // and front face...
-    -0.5, -0.5, 0.5,  1.0, 0.0, 1.0,
-    0.5,  -0.5, 0.5,  0.0, 1.0, 1.0,
-    0.5,  0.5,  0.5,  1.0, 1.0, 1.0,
-    -0.5, 0.5,  0.5,  0.2, 0.2, 0.2,
-};
-
-const cubeIndices = [_]u32{
-    0, 1, 2, 2, 3, 0, // back
-    4, 5, 6, 6, 7, 4, // front
-    4, 5, 1, 1, 0, 4, // bottom
-    7, 6, 2, 2, 3, 7, // top
-    5, 6, 2, 2, 1, 5, // right
-    4, 7, 3, 3, 0, 4, // left
-};
--- src/shader/vert.glsl
+++ src/shader/vert.glsl
@@ -1,14 +1,7 @@
 #version 330 core
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
+layout (location = 0) in vec3 aPos;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-out vec3 vColor;
-
-void main() {
-    vColor = aColor;
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
